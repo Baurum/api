23df2bf9ed9f36f348ab5d8d043ec69c51bdcebb
Marcos Bau <marcosbau5@gmail.com>
2019-11-15 09:12:41 +0100

    Add check token access

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -7,21 +7,29 @@ const User = require('../models/user');
  * @param res
  */
 exports.create = function login(req, res) {
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+    //if no token found, response with no token provided
+    if (!token) return res.status(401).send("Access denied. No token provided.");
     if(!req.body.email || !req.body.password) {
         res.status(400).send('Missing credentials');
         return;
     }
     User.findOne({email: req.body.email}, (err, user) => {
         if(user !== null) {
-            if(err === null) return res.status(422).send('This user not exist' );
-            if(err || req.body.password !== user.password) return res.status(422).send('Invalid credentials' );
-        } else {
-            const session = new Session();
-            session.userId = req.body.email;
-            session.save((err, session) => {
+            if(err !== null) return res.status(422).send('This user not exist' );
+            if(req.body.password !== user.password) return res.status(422).send('Invalid credentials' );
+            if (token === user.id) {
+                const session = new Session();
+                session.userId = req.body.email;
+                session.save((err, session) => {
                     if (err) return res.status(422).send('Ops this is embarrassing');
                     return res.status(200).send(session)
                 });
+            } else {
+                return res.send(403)
+            }
+        } else {
+                return res.send('user null')
         }
     });
 
@@ -33,7 +41,13 @@ exports.create = function login(req, res) {
  * @param res
  */
 exports.delete = function logout(req, res) {
-    Session.deleteOne({userId: req.body.email}, function (err) {
-        res.status(205).send('Logout')
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+    //if no token found, response with no token provided
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+    User.findById(token, (err) => {
+        if(err) return res.status(401).send("Access denied. No token provided.");
+        Session.deleteOne({userId: req.body.email}, function (err) {
+            res.status(205).send('Logout')
+        });
     });
 };
--- controllers/time-registries.js
+++ controllers/time-registries.js
@@ -1,4 +1,6 @@
 const TimeRegistry = require('../models/time-registry');
+const User = require('../models/user');
+
 
 /**
  * Get time registry listing.
@@ -6,9 +8,14 @@ const TimeRegistry = require('../models/time-registry');
  * @param res
  */
 exports.index = function index(req, res) {
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+    //if no token found, response with no token provided
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+    if (token !== req.body.userId) return res.status(403).send("Access denied. No token provided.");
     TimeRegistry.find({userId: req.body.userId}, (err, timeRegistries) => {
         if(err) return res.status(404).send('Not found');
         return res.status(200).send(timeRegistries)
+
     })
 };
 
@@ -18,14 +25,24 @@ exports.index = function index(req, res) {
  * @param res
  */
 exports.create = function create(req, res) {
-    const newRegistry = new TimeRegistry();
-    newRegistry.userId = req.body.userId;
-    newRegistry.startDate = req.body.startDate;
-    newRegistry.endDate = req.body.endDate;
-    newRegistry.entryTime = req.body.entryTime;
-    newRegistry.exitTime = req.body.exitTime;
-    newRegistry.save((err, registry) => {
-        if (err) return res.status(422).send('Something was wrong');
-        return res.status(201).send(registry)
+    const token = req.headers["x-access-token"] || req.headers["authorization"];
+    //if no token found, response with no token provided
+    if (!token) return res.status(401).send("Access denied. No token provided.");
+    User.findById(token, (err) => {
+        if(err) return res.status(401).send("Access denied. No token provided.");
+        if(token === req.body.userId) {
+            const newRegistry = new TimeRegistry();
+            newRegistry.userId = req.body.userId;
+            newRegistry.startDate = req.body.startDate;
+            newRegistry.endDate = req.body.endDate;
+            newRegistry.entryTime = req.body.entryTime;
+            newRegistry.exitTime = req.body.exitTime;
+            newRegistry.save((err, registry) => {
+                if (err) return res.status(422).send('Something was wrong');
+                return res.status(201).send(registry)
+            });
+        } else {
+            return res.status(401)
+        }
     });
 };
